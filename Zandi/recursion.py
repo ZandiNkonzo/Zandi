d1214d6070a52c6fd359105783724bc2b80f9f51
ZandiNkonzo <48716295+ZandiNkonzo@users.noreply.github.com>
2019-03-19 15:09:04 +0200

    Update recursion.py

--- Zandi/recursion.py
+++ Zandi/recursion.py
@@ -13,7 +13,7 @@ def fibonacci(n):
         return fibonacci(n - 2) + fibonacci(n - 1)
 def factorial(n):
     if n<=1:
-        return n
+        return 1
     else:
         return n*factorial(n-1)
 
